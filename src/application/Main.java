3e400053bef4be94c7a06b2617139cfb7d9fa854
Timothy Lesgaux <cph550@my.utsa.edu>
2021-03-25 19:11:10 -0500

    added my name

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-
+// My name is ...
 
 public class Main extends Application {
 	@Override
