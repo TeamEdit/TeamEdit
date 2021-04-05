a8854f8c82743ec42ae768e0efc05423e17f38e8
Timothy Lesgaux <cph550@my.utsa.edu>
2021-04-04 20:21:51 -0500

    Hooked up FileManager.fxml with controller

--- src/application/FileManagerController.java
+++ src/application/FileManagerController.java
@@ -4,7 +4,12 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class FileManagerController implements Initializable {
@@ -14,9 +19,21 @@ public class FileManagerController implements Initializable {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	@FXML
+	private Button openFile;
+	
 	@FXML
 	private void loadEditor(ActionEvent event) {
-		;
+		try{
+			Stage stage = (Stage) openFile.getScene().getWindow();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Editor.fxml"));
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
