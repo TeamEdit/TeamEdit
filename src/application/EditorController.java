023f610d0858abf27029389af54aab2c6bca56f1
grelyelo <32376762+grelyelo@users.noreply.github.com>
2021-04-24 19:40:43 -0500

    added stub function for loading selected files into the view

--- src/application/EditorController.java
+++ src/application/EditorController.java
@@ -1,4 +1,6 @@
 package application;
+import java.nio.file.Path;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -35,4 +37,16 @@ public class EditorController {
 
 	}
 	
+    @FXML
+    void initialize() { // Is called when Editor.fxml loads the EditorController.java 
+    	/* Required in order to display the songs in the pane when the FileManagerController loads the EditorController. 
+    	For now, we are simply loading the filename in place of the "song name" field
+    	Eventually, once I get the code working to parse tags, I'll make the tag parser read in 
+    	all the tags for each song, and represent those internally as some Song object. 
+    	This is just to get my code 'working' for now. */ 
+    	Path selected = Main.filesystem.getSelectedDir();
+    	if(selected != null)
+    		System.out.println("About to load song: " + selected);
+    	
+    }
 }
